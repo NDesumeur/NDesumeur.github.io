5dd162d13e2869cdc028affb8129b91a34a64a81
NDesumeur <desumeur.nolan@gmail.com>
2024-03-05 01:48:44 +0100

    logo

--- appli.py
+++ appli.py
@@ -60,6 +60,7 @@ def generate_anime_html_page(title, image_name, note_finale, anime_link, genres,
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="logo.png">
     <link rel="stylesheet" href="anime.css">
     <title>{title}</title>
 </head>
@@ -98,6 +99,7 @@ def generate_film_html_page(title, image_name, critique, synopsis, review_date, 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="anime.css">
+    <link rel="icon" type="image/png" href="logo.png">
     <title>{title}</title>
 </head>
 <body>
